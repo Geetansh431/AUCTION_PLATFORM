0370e105e073e51e60059045f98cc5f7ed3d602f
geetansh431 <geetanshg2@gmail.com>
2025-01-29 00:36:19 +0530

    Added MontlyRevenue/FetchUserDetails

--- backend/router/superAdminRoutes.js
+++ backend/router/superAdminRoutes.js
@@ -3,8 +3,10 @@ import { isAuthenticated, isAuthorized } from "../middlewares/auth.js";
 import {
     deleteAuctionItem,
     deletePaymentProof,
+    fetchAllUsers,
     getAllPaymentProofs,
     getPaymentProofDetail,
+    monthlyRevenue,
     updateProofStatus
 } from "../controllers/superAdminController.js";
 
@@ -15,6 +17,6 @@ router.get("/paymentproofs/getall", isAuthenticated, isAuthorized("Super Admin")
 router.get("/paymentproof/:id", isAuthenticated, isAuthorized("Super Admin"), getPaymentProofDetail);
 router.put("/paymentproof/status/update/:id", isAuthenticated, isAuthorized("Super Admin"), updateProofStatus);
 router.delete("/paymentproof/delete/:id", isAuthenticated, isAuthorized("Super Admin"), deletePaymentProof);
-
-
+router.get("/users/getall", isAuthenticated, isAuthorized("Super Admin"), fetchAllUsers);
+router.get("/monthlyincome", isAuthenticated, isAuthorized("Super Admin"), monthlyRevenue);
 export default router;
