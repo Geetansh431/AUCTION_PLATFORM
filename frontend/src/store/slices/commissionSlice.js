14b5af787f8d1824e89da2caebbaca738c279e4a
geetansh431 <geetanshg2@gmail.com>
2025-04-28 07:37:19 +0530

    Hopefully Final Deployment

--- frontend/src/store/slices/commissionSlice.js
+++ frontend/src/store/slices/commissionSlice.js
@@ -24,7 +24,7 @@ export const postCommissionProof = (data) => async (dispatch) => {
     dispatch(commissionSlice.actions.postCommissionProofRequest());
     try {
         const response = await axios.post(
-            "http://localhost:4000/api/v1/commission/proof",
+            "https://auction-platform-1asl.onrender.com/api/v1/commission/proof",
             data,
             {
                 withCredentials: true,
